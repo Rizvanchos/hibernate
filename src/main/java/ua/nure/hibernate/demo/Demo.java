a5bc9936f44477a40cf80938155de9d89a34b221
Rizvan Hasanov <rizvanchos@mail.ru>
2016-09-05 16:37:40 +0300

    Hibernate inheritance examples

--- src/main/java/ua/nure/hibernate/demo/Demo.java
+++ src/main/java/ua/nure/hibernate/demo/Demo.java
@@ -3,33 +3,33 @@ package ua.nure.hibernate.demo;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import ua.nure.hibernate.entity.User;
+import ua.nure.hibernate.entity.FourWheeler;
+import ua.nure.hibernate.entity.TwoWheeler;
 import ua.nure.hibernate.entity.Vehicle;
 
 import java.io.IOException;
-import java.util.Date;
 
 public class Demo {
 
     public static void main(String[] args) throws IOException {
-        User user = new User();
-        user.setUsername("User");
-        user.setBirthday(new Date());
-        user.setDescription("User description");
-
         Vehicle car = new Vehicle();
         car.setName("Car");
-        Vehicle bus = new Vehicle();
-        bus.setName("Bus");
 
-        user.getVehicles().add(car);
-        user.getVehicles().add(bus);
+        TwoWheeler bike = new TwoWheeler();
+        bike.setName("Bike");
+        bike.setSteeringHandle("Bike Steering handle");
+
+        FourWheeler porsche = new FourWheeler();
+        porsche.setName("Porsche");
+        porsche.setSteeringWheel("Porsche Steering wheel");
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        session.persist(user);
+        session.save(car);
+        session.save(bike);
+        session.save(porsche);
 
         session.getTransaction().commit();
         session.close();
---
+++ src/main/java/ua/nure/hibernate/entity/TwoWheeler.java
@@ -0,0 +1,17 @@
+package ua.nure.hibernate.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class TwoWheeler extends Vehicle {
+
+    private String steeringHandle;
+
+    public String getSteeringHandle() {
+        return steeringHandle;
+    }
+
+    public void setSteeringHandle(String steeringHandle) {
+        this.steeringHandle = steeringHandle;
+    }
+}
