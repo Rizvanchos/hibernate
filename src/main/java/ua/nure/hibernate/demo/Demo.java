d1fb9ce67aed27f8667993894a353728a926ea35
Rizvan Hasanov <rizvanchos@mail.ru>
2016-09-06 16:15:46 +0300

    First level cache

--- src/main/java/ua/nure/hibernate/demo/Demo.java
+++ src/main/java/ua/nure/hibernate/demo/Demo.java
@@ -3,14 +3,9 @@ package ua.nure.hibernate.demo;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.criterion.Example;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
 import ua.nure.hibernate.entity.User;
 
 import java.io.IOException;
-import java.util.List;
 
 public class Demo {
 
@@ -20,52 +15,21 @@ public class Demo {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        // **********************************************************
+        // *********************************************************
+        User firstUser = (User) session.get(User.class, 1);
+        User secondUser = (User) session.get(User.class, 1); // only one select (first level cache)
 
-        List<User> users = session.createCriteria(User.class)
-                .add(Restrictions.or(Restrictions.between("id", 0, 3), Restrictions.between("id", 5, 8)))
-                .addOrder(Order.desc("id"))
-                .list();
+        firstUser.setUsername("Updated name"); // update query goes
 
-        for (User user : users) {
-            System.out.println(user.getUsername());
-        }
+        secondUser = (User)session.get(User.class, 1); // no select (already in cache)
 
-        // **********************************************************
+        session.getTransaction().commit();
+        session.close();
 
-        List<Integer> maxId = session.createCriteria(User.class).setProjection(Projections.max("id")).list();
+        session = sessionFactory.openSession();
+        session.beginTransaction();
 
-        for (Integer max : maxId) {
-            System.out.println(max);
-        }
-
-        // **********************************************************
-
-        User userExample = new User();
-        userExample.setId(5); // hibernate ignores primary keys and null properties
-        userExample.setUsername("User 5");
-
-        Example example = Example.create(userExample);
-
-        users = session.createCriteria(User.class).add(example).list();
-
-        for (User user : users) {
-            System.out.println(user.getUsername());
-        }
-
-        // **********************************************************
-
-        userExample.setUsername("User%");
-
-        example = Example.create(userExample).enableLike();
-
-        users = session.createCriteria(User.class).add(example).list();
-
-        for (User user : users) {
-            System.out.println(user.getUsername());
-        }
-
-        // **********************************************************
+        secondUser = (User)session.get(User.class, 1); // select executes (first level cache works only during session)
 
         session.getTransaction().commit();
         session.close();
