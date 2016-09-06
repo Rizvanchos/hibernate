b826d4b372899804281eaa96c490e90e516e94ef
Rizvan Hasanov <rizvanchos@mail.ru>
2016-09-06 15:47:51 +0300

    Projection and Query by example

--- src/main/java/ua/nure/hibernate/demo/Demo.java
+++ src/main/java/ua/nure/hibernate/demo/Demo.java
@@ -1,9 +1,11 @@
 package ua.nure.hibernate.demo;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import ua.nure.hibernate.entity.User;
 
@@ -20,10 +22,44 @@ public class Demo {
 
         // **********************************************************
 
-        Criteria criteria = session.createCriteria(User.class);
-        criteria.add(Restrictions.eq("username", "User 10"));
+        List<User> users = session.createCriteria(User.class)
+                .add(Restrictions.or(Restrictions.between("id", 0, 3), Restrictions.between("id", 5, 8)))
+                .addOrder(Order.desc("id"))
+                .list();
 
-        List<User> users = criteria.list();
+        for (User user : users) {
+            System.out.println(user.getUsername());
+        }
+
+        // **********************************************************
+
+        List<Integer> maxId = session.createCriteria(User.class).setProjection(Projections.max("id")).list();
+
+        for (Integer max : maxId) {
+            System.out.println(max);
+        }
+
+        // **********************************************************
+
+        User userExample = new User();
+        userExample.setId(5); // hibernate ignores primary keys and null properties
+        userExample.setUsername("User 5");
+
+        Example example = Example.create(userExample);
+
+        users = session.createCriteria(User.class).add(example).list();
+
+        for (User user : users) {
+            System.out.println(user.getUsername());
+        }
+
+        // **********************************************************
+
+        userExample.setUsername("User%");
+
+        example = Example.create(userExample).enableLike();
+
+        users = session.createCriteria(User.class).add(example).list();
 
         for (User user : users) {
             System.out.println(user.getUsername());
